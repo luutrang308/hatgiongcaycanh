1380c36c6914a7d5ebb7afe05510d91de9659325
luutrang308 <luutrangk10@gmail.com>
2020-09-12 15:08:46 +0700

    Trang

--- assets/js/custom-tr.js
+++ assets/js/custom-tr.js
@@ -1,63 +1,39 @@
+$(document).ready(function(){
+  $(".video-for .video-item .title").hover(function(){
+    $(this).css("display", "none");
+  });
+});
 /* slider post */
 $('.slider-post').slick({
-  dots: true,/*
-  infinite: true,
-  speed: 300,
-  autoplaySpeed: 1200,*/
-  slidesToShow: 3,/*
-  adaptiveHeight: true,
-  autoplay: true,*/
+  dots: true,
+  slidesToShow: 3,
+  /*autoplay: true,*/
+});
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  $('.slider-post').slick('setPosition');
 });
 
-/* countdown timer */
-const second = 1000,
-     minute = second * 60,
-     hour = minute * 60,
-     day = hour * 24;
-
-let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
-    x = setInterval(function() {   
-    let now = new Date().getTime(),
-    	distance = countDown - now;
-    document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-}, second);
-/* countdown timer */
 /* slider homepage */
 $('.slider-pc').slick({
-  dots: true,/*
-  infinite: true,
-  speed: 300,
-  autoplaySpeed: 1200,*/
-  slidesToShow: 1,/*
-  adaptiveHeight: true,
-  autoplay: true,*/
+  dots: true,
+  slidesToShow: 1,
+  autoplay: true,
   prevArrow:"<button class='prev slick-prev'><img class='left-arrow ' src='assets/img/left.png' alt=''></button>",
   nextArrow:"<button class='next slick-next'><img class='right-arrow ' src='assets/img/right.png' alt=''></button>",
 });
 
 /* slide product homepage */
 $('.slider-product').slick({
-  dots: true,/*
-  infinite: true,
-  speed: 300,
-  autoplaySpeed: 1200,*/
-  slidesToShow: 4,/*
-  adaptiveHeight: true,
-  autoplay: true,*/
+  dots: true,
+  slidesToShow: 4,
+  /*autoplay: true,*/
 });
 
 /* slide new product homepage */
 $('.slider-new-product').slick({
-  dots: true,/*
-  infinite: true,
-  speed: 300,
-  autoplaySpeed: 1200,*/
-  slidesToShow: 4,/*
-  adaptiveHeight: true,
-  autoplay: true,*/
+  dots: true,
+  slidesToShow: 4,
+  /*autoplay: true,*/
 });
 
 /* slide new product */
